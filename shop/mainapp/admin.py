f4e54bbe34d9234f226feea0efbf77e95d54b8ea
Евгений Кулеша <tms-21-online/>
2021-10-19 00:16:02 +0300

    Static and media enable in settings and urls

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,12 +1,28 @@
+from django.forms import ModelChoiceField
 from django.contrib import admin
-
-# Register your models here.
 
 from .models import *
 
+
+class FishRodAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='FishRods'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class FishLineAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='FishLines'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
 admin.site.register(Category)
-admin.site.register(FishRod)
-admin.site.register(FishLine)
+admin.site.register(FishRod, FishRodAdmin)
+admin.site.register(FishLine, FishLineAdmin)
 admin.site.register(CartProduct)
 admin.site.register(Cart)
 admin.site.register(Customer)
