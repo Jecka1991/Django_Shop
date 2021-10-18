5a6196ad5e3be8501c40c8a3232242c49c6a5482
Евгений Кулеша <tms-21-online/>
2021-10-19 00:46:02 +0300

    Products of different models in one by using ContentType

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -5,16 +5,28 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 
 User = get_user_model()
 
-#**********
-#1 Category
-#2 Product
-#3 CartProduct
-#4 Cart
-#5 Order
-#**********
-#6 Customer
-#7 Specification
 
+class LatestProductManager:
+
+    @staticmethod
+    def get_products_for_main_page(*args, **kwargs):
+        with_respect_to = kwargs.get('with_respect_to')
+        products = []
+        ct_models = ContentType.objects.filter(model__in=args)
+        for ct_model in ct_models:
+            model_products = ct_model.model_class()._base_manager.all().order_by('-id')[:5]
+            products.extend(model_products)
+        if with_respect_to:
+            ct_model = ContentType.objects.filter(model=with_respect_to)
+            if ct_model.exists():
+                if with_respect_to in args:
+                    return sorted(products, key=lambda x: x.__class__._meta.model.name.startswith(with_respect_to), reverse=True)
+        return products
+
+
+class LatestProducts:
+
+    object = LatestProductManager
 
 class Category(models.Model):
 
