886d0b12d81789f7303f590298a3a9aa4827a0fe
Евгений Кулеша <tms-21-online/>
2021-10-22 16:18:47 +0300

    Аix 'Categories' using mixins

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, View
 
 from .models import FishRod, FishLine, Category
+from .mixins import CategoryDetailMixin
 
 
-def test_view(request):
+class BaseView(View):
 
-    categories = Category.objects.get_categories_for_left_sidebar()
-    return render(request, 'base.html', {'categories': categories})
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.get_categories_for_left_sidebar()
+        return render(request, 'base.html', {'categories': categories})
+# def test_view(request):
+#
+#     categories = Category.objects.get_categories_for_left_sidebar()
+#     return render(request, 'base.html', {'categories': categories})
 
 
-class ProductDetailView(DetailView):
+class ProductDetailView(CategoryDetailMixin, DetailView):
 
     CT_MODEL_MODEL_CLASS = {
         'fishrod': FishRod,
@@ -27,7 +33,7 @@ class ProductDetailView(DetailView):
     slug_url_kwarg = 'slug'
 
 
-class CategoryDetailView(DetailView):
+class CategoryDetailView(CategoryDetailMixin, DetailView):
 
     model = Category
     queryset = Category.objects.all()
