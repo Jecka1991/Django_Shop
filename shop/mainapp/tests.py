8b0bfda7410d0134b2e256a141fc7ede6c199a28
Евгений Кулеша <tms-21-online/>
2021-10-27 19:23:55 +0300

    tests

--- shop/mainapp/tests.py
+++ shop/mainapp/tests.py
@@ -1,3 +1,61 @@
-from django.test import TestCase
+from decimal import Decimal
+from unittest import mock
+from django.test import TestCase, RequestFactory
+from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
 
-# Create your tests here.
+from .models import Category, FishRod, CartProduct, Cart, Customer
+from .views import recalc_cart, AddToCartView, BaseView
+
+User = get_user_model()
+
+
+class ShopTestCases(TestCase):
+
+    def setUp(self) -> None:
+        self.user = User.objects.create(username='testuser', password='password')
+        self.category = Category.objects.create(name='Удочки', slug='FishRods')
+        image = SimpleUploadedFile("fishrod_image.jpg", content=b'', content_type="image/jpg")
+        self.fishrod = FishRod.objects.create(
+            category=self.category,
+            title="Test FishRod",
+            slug="test-slug",
+            image=image,
+            price=Decimal('100.00'),
+            length="3,6 м",
+            weight="293 г",
+            number_of_sections="4",
+            maximum_test="150 г",
+            transport_length="149 см"
+        )
+        self.customer = Customer.objects.create(user=self.user, phone="1111", address="Adr")
+        self.cart = Cart.objects.create(owner=self.customer)
+        self.cart_product = CartProduct.objects.create(
+            user=self.customer,
+            cart=self.cart,
+            content_object=self.fishrod
+        )
+
+    def test_add_to_cart(self):
+        self.cart.products.add(self.cart_product)
+        recalc_cart(self.cart)
+        self.assertIn(self.cart_product, self.cart.products.all())
+        self.assertEqual(self.cart.products.count(), 1)
+        self.assertEqual(self.cart.final_price, Decimal('100.00'))
+
+    def test_response_from_add_to_cart_view(self):
+        factory = RequestFactory()
+        request = factory.get('')
+        request.user = self.user
+        response = AddToCartView.as_view()(request, ct_model="fishrod", slug="test-slug")
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/cart/')
+
+    def test_mock_homepage(self):
+        mock_data = mock.Mock(status_code=444)
+        with mock.patch('mainapp.views.BaseView.get', return_value=mock_data) as mock_data:
+            factory = RequestFactory()
+            request = factory.get('')
+            request.user = self.user
+            response = BaseView.as_view()(request)
+            self.assertEqual(response.status_code, 444)
