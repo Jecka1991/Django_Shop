4714ac57cf0e7f0a067a76f0de900a21f642d020
Евгений Кулеша <tms-21-online/>
2021-10-28 23:51:01 +0300

    Django user authorization

--- shop/mainapp/urls.py
+++ shop/mainapp/urls.py
@@ -9,6 +9,7 @@ from .views import (
     ChangeQTYView,
     CheckoutView,
     MakeOrderView,
+    LoginView,
 )
 
 
@@ -21,6 +22,7 @@ urlpatterns = [
     path('remove-from-cart/<str:slug>/', DeleteFromCartView.as_view(), name='delete_from_cart'),
     path('change-qty/<str:slug>/', ChangeQTYView.as_view(), name='change_qty'),
     path('checkout/', CheckoutView.as_view(), name='checkout'),
-    path('make-order', MakeOrderView.as_view(), name='make_order')
+    path('make-order', MakeOrderView.as_view(), name='make_order'),
+    path('login/', LoginView.as_view(), name='login')
 ]
 
