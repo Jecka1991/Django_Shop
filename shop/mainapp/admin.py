5e4160bfaf801dbfb39b6f3adeb985d8d07eb894
Евгений Кулеша <tms-21-online/>
2021-10-28 21:42:46 +0300

    Refactoring: change the architecture of the project, remove unnecessary models, clean up views and templates

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,56 +1,11 @@
-from django.forms import ModelChoiceField, ModelForm, ValidationError
 from django.contrib import admin
-from django.utils.safestring import mark_safe
 
 from .models import *
 
 
-class FishRodAdminForm(ModelForm):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['image'].help_text = mark_safe(
-            '<span style="color:red; font-size:14px;">Загружайте изображение с минимальным разрешением {}X{}</span> '.format(
-                *Product.MIN_RESOLUTION
-            )
-        )
-
-    def clean_image(self):
-        image = self.cleaned_data['image']
-        img = Image.open(image)
-        min_width, min_height = Product.MIN_RESOLUTION
-        max_width, max_height = Product.MAX_RESOLUTION
-        if image.size > Product.MAX_IMAGE_SIZE:
-            raise ValidationError('Размер изображения не должен превышать 3МБ!')
-        if img.height < min_height or img.width < min_width:
-            raise ValidationError('Разрешение изображения меньше минимального!')
-        if img.height > max_height or img.width > max_width:
-            raise ValidationError('Разрешение изображения больше максимального!')
-        return image
-
-
-class FishRodAdmin(admin.ModelAdmin):
-
-    form = FishRodAdminForm
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == 'category':
-            return ModelChoiceField(Category.objects.filter(slug='FishRods'))
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
-
-
-class FishLineAdmin(admin.ModelAdmin):
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == 'category':
-            return ModelChoiceField(Category.objects.filter(slug='FishLines'))
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
-
-
 admin.site.register(Category)
-admin.site.register(FishRod, FishRodAdmin)
-admin.site.register(FishLine, FishLineAdmin)
 admin.site.register(CartProduct)
 admin.site.register(Cart)
 admin.site.register(Customer)
 admin.site.register(Order)
+admin.site.register(Product)
--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -1,80 +1,16 @@
-from PIL import Image
-
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes.fields import GenericForeignKey
 from django.urls import reverse
 from django.utils import timezone
 
 User = get_user_model()
 
 
-def get_models_for_count(*model_names):
-    return [models.Count(model_name) for model_name in model_names]
-
-
-def get_product_url(obj, viewname):
-    ct_model = obj.__class__._meta.model_name
-    return reverse(viewname, kwargs={'ct_model': ct_model, 'slug': obj.slug})
-
-
-class MinResolutionErrorException(Exception):
-    pass
-
-
-class MaxResolutionErrorException(Exception):
-    pass
-
-
-class LatestProductManager:
-
-    @staticmethod
-    def get_products_for_main_page(*args, **kwargs):
-        with_respect_to = kwargs.get('with_respect_to')
-        products = []
-        ct_models = ContentType.objects.filter(model__in=args)
-        for ct_model in ct_models:
-            model_products = ct_model.model_class()._base_manager.all().order_by('-id')[:5]
-            products.extend(model_products)
-        if with_respect_to:
-            ct_model = ContentType.objects.filter(model=with_respect_to)
-            if ct_model.exists():
-                if with_respect_to in args:
-                    return sorted(products, key=lambda x: x.__class__._meta.model.name.startswith(with_respect_to), reverse=True)
-        return products
-
-
-class LatestProducts:
-
-    objects = LatestProductManager
-
-
-class CategoryManager(models.Manager):
-
-    CATEGORY_NAME_COUNT_NAME = {
-        'Удочки': 'fishrod__count',
-        'Леска': 'fishline__count'
-    }
-
-    def get_queryset(self):
-        return super().get_queryset()
-
-    def get_categories_for_left_sidebar(self):
-        models = get_models_for_count('fishrod', 'fishline')
-        qs = list(self.get_queryset().annotate(*models))
-        data = [
-            dict(name=c.name, url=c.get_absolute_url(), count=getattr(c, self.CATEGORY_NAME_COUNT_NAME[c.name]))
-            for c in qs
-        ]
-        return data
-
-
 class Category(models.Model):
 
     name = models.CharField(max_length=255, verbose_name='Имя категории')
     slug = models.SlugField(unique=True)
-    objects = CategoryManager()
 
     def __str__(self):
         return self.name
@@ -89,10 +25,7 @@ class Product(models.Model):
     MAX_RESOLUTION = (1200, 1200)
     MAX_IMAGE_SIZE = 3145728
 
-    class Meta:
-        abstract = True
-
-    category = models.ForeignKey(Category, verbose_name='Категоря', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
     title = models.CharField(max_length=255, verbose_name='Наименование')
     slug = models.SlugField(unique=True)
     image = models.ImageField(verbose_name='Изображение')
@@ -102,66 +35,26 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
-    def save(self, *args, **kwargs):
-        image = self.image
-        img = Image.open(image)
-        min_width, min_height = Product.MIN_RESOLUTION
-        max_width, max_height = Product.MAX_RESOLUTION
-        if img.height < min_height or img.width < min_width:
-            raise MinResolutionErrorException('Разрешение изображения меньше минимального!')
-        if img.height > max_height or img.width > max_width:
-            raise MaxResolutionErrorException('Разрешение изображения больше максимального!')
-        super().save(*args, **kwargs)
-
     def get_model_name(self):
         return self.__class__.__name__.lower()
 
-
-class FishRod(Product):
-
-    type = models.CharField(max_length=255, verbose_name='Тип удилища')
-    length = models.CharField(max_length=255, verbose_name='Длина удилища')
-    weight = models.CharField(max_length=255, verbose_name='Вес удилища')
-    transport_length = models.CharField(max_length=255, verbose_name='Транспортировочная длина')
-    number_of_sections = models.CharField(max_length=255, verbose_name='Количество секций')
-    maximum_test = models.CharField(max_length=255, verbose_name='Максимальный тест')
-
-    def __str__(self):
-        return "{} : {}".format(self.category.name, self.title)
-
     def get_absolute_url(self):
-        return get_product_url(self, 'product_detail')
-
-
-class FishLine(Product):
-    type = models.CharField(max_length=255, verbose_name='Тип лески')
-    diameter = models.CharField(max_length=255, verbose_name='Диаметр лески')
-    breaking_load = models.CharField(max_length=255, verbose_name='Разрывная нагрузка')
-    length = models.CharField(max_length=255, verbose_name='Длина лески')
-    color = models.CharField(max_length=255, verbose_name='Цвет лески')
-
-    def __str__(self):
-        return "{} : {}".format(self.category.name, self.title)
-
-    def get_absolute_url(self):
-        return get_product_url(self, 'product_detail')
+        return reverse('product_detail', kwargs={'slug': self.slug})
 
 
 class CartProduct(models.Model):
 
     user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)
     cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE, related_name='related_products')
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey('content_type', 'object_id')
+    product = models.ForeignKey(Product, verbose_name='Товар', on_delete=models.CASCADE)
     qty = models.PositiveIntegerField(default=1)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая стоимость')
 
     def __str__(self):
-        return "Продукт: {} (для корзины)".format(self.content_object.title)
+        return "Продукт: {} (для корзины)".format(self.product.title)
 
     def save(self, *args, **kwargs):
-        self.final_price = self.qty * self.content_object.price
+        self.final_price = self.qty * self.product.price
         super().save(*args, **kwargs)
 
 
