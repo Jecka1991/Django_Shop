0a4d3f8c605137e4ec752699050338f45ebbfd14
Евгений Кулеша <tms-21-online/>
2021-10-31 23:31:56 +0300

    create new application, transfer models

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -18,18 +18,8 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('category_detail', kwargs={'slug': self.slug})
 
-    def get_fields_for_filter_in_template(self):
-        return ProductFeatures.objects.filter(
-            category=self,
-            use_in_filter=True
-        ).prefetch_related('category').value('feature_key', 'feature_measure', 'feature_name', 'filter_type')
-
 
 class Product(models.Model):
-
-    MIN_RESOLUTION = (400, 400)
-    MAX_RESOLUTION = (1200, 1200)
-    MAX_IMAGE_SIZE = 3145728
 
     category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
     title = models.CharField(max_length=255, verbose_name='Наименование')
@@ -37,74 +27,16 @@ class Product(models.Model):
     image = models.ImageField(verbose_name='Изображение')
     description = models.TextField(verbose_name='Описание', null=True)
     price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Цена')
+    characteristics = models.ManyToManyField("specifications.ProductCharacteristics", blank=True, related_name='characteristic_for_product')
 
     def __str__(self):
         return self.title
 
-    def get_model_name(self):
-        return self.__class__.__name__.lower()
-
     def get_absolute_url(self):
         return reverse('product_detail', kwargs={'slug': self.slug})
 
-
-class ProductFeatures(models.Model):
-
-    RADIO = 'radio'
-    CHECKBOX = 'checkbox'
-
-    FILTER_TYPE_CHOICES = (
-        (RADIO, 'Радиокнопка'),
-        (CHECKBOX, 'Чекбокс')
-    )
-    feature_key = models.CharField(max_length=100, verbose_name='Ключ характеристики')
-    feature_key = models.CharField(max_length=255, verbose_name='Наименование характеристики')
-    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
-    postfix_for_value = models.CharField(
-        max_length=20,
-        null=True,
-        blank=True,
-        verbose_name='Постфикс для значения',
-        help_text=f'Например для характеристики "Часы работы" к значению можно добавить постфикс "часов",'
-                  f' и как результат - значение "10 часов"'
-    )
-    use_in_filter = models.BooleanField(
-        default=False,
-        verbose_name='Использовать в фильтрации товаров в шаблоне',
-    )
-    filter_type = models.CharField(
-        max_length=20,
-        verbose_name='Тип фильтра',
-        default=CHECKBOX,
-        choices=FILTER_TYPE_CHOICES
-    )
-    filter_measure = models.CharField(
-        max_length=50,
-        verbose_name='Единица измерения для фильтра',
-        help_text='Единица измерения для конкретного фильтра.'
-                  'Например "Длина лески (м)". Единицей измерения будет информация в скобках.'
-    )
-
-    def __str__(self):
-        return f'Категория - "{self.category.name}" | Характеристика - "{self.feature_name}"'
-
-
-class ProductFeatureValidators(models.Model):
-
-    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
-    feature = models.ForeignKey(
-        ProductFeatures, verbose_name='Характеристика', null=True, blank=True, on_delete=models.CASCADE
-    )
-    feature_value = models.CharField(
-        max_length=255, unique=True, null=True, blank=True, verbose_name='Значение характеристики'
-    )
-
-    def __str__(self):
-        if not self.feature:
-            return f'Валидатор категории "{self.category.name}" - характеристика не выбрана'
-        return f'Валидатор категории "{self.category.name}" |' \
-               f'Характеристика - "{self.feature.feature_name}" |' \
-               f'Значение - "{self.feature_value}"'
+    def get_characteristics(self):
+        return {f.characteristic.characteristic_name: ' '.join([f.value, f.characteristic.unit or ""]) for f in self.characteristic.all()}
 
 
 class CartProduct(models.Model):
@@ -113,7 +45,7 @@ class CartProduct(models.Model):
     cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE, related_name='related_products')
     product = models.ForeignKey(Product, verbose_name='Товар', on_delete=models.CASCADE)
     qty = models.PositiveIntegerField(default=1)
-    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая стоимость')
+    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
 
     def __str__(self):
         return "Продукт: {} (для корзины)".format(self.product.title)
@@ -128,7 +60,7 @@ class Cart(models.Model):
     owner = models.ForeignKey('Customer', null=True, verbose_name='Владелец', on_delete=models.CASCADE)
     products = models.ManyToManyField(CartProduct, blank=True, related_name='related_cart')
     total_products = models.PositiveIntegerField(default=0)
-    final_price = models.DecimalField(max_digits=9, default=0, decimal_places=2, verbose_name='Общая стоимость')
+    final_price = models.DecimalField(max_digits=9, default=0, decimal_places=2, verbose_name='Общая цена')
     in_order = models.BooleanField(default=False)
     for_anonymous_user = models.BooleanField(default=False)
 
@@ -141,7 +73,7 @@ class Customer(models.Model):
     user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
     phone = models.CharField(max_length=20, verbose_name='Номер телефона', null=True, blank=True)
     address = models.CharField(max_length=255, verbose_name='Адрес', null=True, blank=True)
-    orders = models.ManyToManyField('Order', verbose_name='Заказы покупателя', related_name='related_customer')
+    orders = models.ManyToManyField('Order', verbose_name='Заказы покупателя', related_name='related_order')
 
     def __str__(self):
         return "Покупатель: {} {}".format(self.user.first_name, self.user.last_name)
@@ -177,7 +109,7 @@ class Order(models.Model):
     address = models.CharField(max_length=1024, verbose_name='Адрес', null=True, blank=True)
     status = models.CharField(
         max_length=100,
-        verbose_name='Статус заказа',
+        verbose_name='Статус заказ',
         choices=STATUS_CHOICES,
         default=STATUS_NEW
     )
@@ -193,3 +125,5 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+
---
+++ shop/specifications/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+class CategoryCharacteristic(models.Model):
+
+    category = models.ForeignKey("mainapp.Category", verbose_name='Категория', on_delete=models.CASCADE)
+    characteristic_name = models.CharField(verbose_name='Название ключа для категории', max_length=50)
+    characteristic_filter_name = models.CharField(verbose_name='Название для фильтра', max_length=50)
+    unit = models.CharField(max_length=50, verbose_name='Единица измерения', null=True, blank=True)
+
+    class Meta:
+        unique_together = ('category', 'characteristic_name', 'characteristic_filter_name')
+
+    def __str__(self):
+        return f"{self.category.name} | {self.characteristic_name}"
+
+
+class CharacteristicValidator(models.Model):
+
+    category = models.ForeignKey("mainapp.Category", verbose_name='Категория', on_delete=models.CASCADE)
+    characteristic_key = models.ForeignKey(CategoryCharacteristic, verbose_name='Ключ характеристики', on_delete=models.CASCADE)
+    valid_characteristic_value = models.CharField(max_length=100, verbose_name='Валидное значение')
+
+    def __str__(self):
+        return f"Категория {self.category.name} | " \
+               f"Характристика {self.characteristic_key.characteristic_name} | " \
+               f"Валидное значение {self.valid_characteristic_value}"
+
+
+class ProductCharacteristics(models.Model):
+
+    product = models.ForeignKey("mainapp.Product", verbose_name='Товар', on_delete=models.CASCADE)
+    characteristic = models.ForeignKey(CategoryCharacteristic, verbose_name='Характеристика', on_delete=models.CASCADE)
+    value = models.CharField(max_length=255, verbose_name='Значение')
+
+    def __str__(self):
+        return f"Товар - {self.product.title} | " \
+               f"Характеристика - {self.characteristic.characteristic_name} | " \
+               f"Значение - {self.value}"
+
